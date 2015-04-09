79f88305b753e81e6eb73e8168d1e6edc65960e0
Juan <juan.antonadelval@gmail.com>
2015-04-09 17:57:36 +0200

    poerative

--- KeyNote.rb
+++ KeyNote.rb
@@ -7,37 +7,56 @@ require "HighLine"
 
 class KeynotePresentation
 
-  def initialize
-    @file_source = "keynote.txt"
-    @arr_text_source
-    @keynote_hash = {}
-    @screen_size = HighLine::SystemExtensions.terminal_size 
+  def initialize(arr_pagination)
+    @screen_size = HighLine::SystemExtensions.terminal_size
+    @arr_pagination = arr_pagination
+    @command
+    @@index = 0
   end	
   
-  def text_process
-   
-    @arr_text_source = IO.read("keynote.txt").split("\n----\n")
-
-    i = 0
-
-    while i < @arr_text_source.length
-      @keynote_hash[@arr_text_source[i]] = i
-      i = i + 1
-    end
-    puts @keynote_hash
-    puts @screen_size	  
-
-  end	
-
-  def load_text
-  end	
-
   def navigation
+    show_page(@arr_pagination, @@index)
+    while @command != "exit"
+      print ">"
+      @command = gets.chomp
+      show_page(@arr_pagination, process_index(@@index, @command))
+    end	
+  end
+  
+  def process_index(index, command)
+    case command
+    when "next"
+      if index < @arr_pagination.length - 1	
+        return @@index = index.next
+      end
+      return @@index
+    when "previous"  
+      if index > 0	
+        return @@index = index.pred
+      end
+      return @@index
+    else
+      return @@index
+    end   
+
+  end
+
+  def show_page(arr, index)
+    puts arr[index]
   end
 
 end 
 
-class Text
+class Text_Process
+  
+  def initialize
+    @file_source = "keynote.txt"
+    @arr_text_source
+  end	
+  
+  def load_text_to_array
+  	return @arr_text_source = IO.read("keynote.txt").split("\n----\n")
+  end
 
 end	
 
@@ -45,6 +64,9 @@ end
 #MAIN
 ##########################
 
-KeynotePresentation.new.text_process
+KeynotePresentation.new(Text_Process.new.load_text_to_array).navigation
+
+
+
 
 
